89ca152e99155fe8edda15b34ba21f87f9c7e2c4
Nguyen Nhat Huy <nguyennhathuy12a1@gmail.com>
2025-12-17 23:05:00 +0700

    Remove compiled JS files from shared

--- shared/options.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const GAME_OPTIONS: readonly ["Rock", "Gun", "Lightning", "Devil", "Dragon", "Water", "Air", "Paper", "Sponge", "Wolf", "Tree", "Human", "Snake", "Scissors", "Fire"];
-export type GameOption = typeof GAME_OPTIONS[number];
-export declare function isValidOption(option: string): option is GameOption;
-export declare function getOptionIndex(option: GameOption): number;
-//# sourceMappingURL=options.d.ts.map
